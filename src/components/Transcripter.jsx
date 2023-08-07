7bb59fedc68bc6ef06686b4d2e2f569615b8f5fa
AWOUNO Kossi Winner <kossi.awouno.infinitus@gmail.com>
2023-08-07 15:00:08 +0000

    qrcode

--- src/components/Transcripter.jsx
+++ src/components/Transcripter.jsx
@@ -1,27 +1,47 @@
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import QRCode from 'qrcode.react';
+import { useState } from 'react'
 
 function Transcripter() {
-    const {
-        transcript,
-        listening,
-        resetTranscript,
-        browserSupportsSpeechRecognition
-      } = useSpeechRecognition();
-    
-      if (!browserSupportsSpeechRecognition) {
-        return <span>Browser doesn't support speech recognition.</span>;
-      }
-   
-      return (
-        <div>
-          <p>Microphone: {listening ? 'on' : 'off'}</p>
-          <button onClick={SpeechRecognition.startListening}>Start</button>
-          <button onClick={SpeechRecognition.stopListening}>Stop</button>
-          <button onClick={SpeechRecognition.startListening({language : 'en-US'})}>Speak in english</button>
-          <button onClick={resetTranscript}>Reset</button>
-          <p>{transcript}</p>
-        </div>
-      );
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  const [QRValue, setQRValue] = useState();
+  const handleQRVlaue = (transcript) => {
+    setQRValue((previousValue) => previousValue = transcript)
+    return true;
+  }
+  const isDefinded = () => {
+    return QRValue != null
+  }
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+  return (
+    <div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      {/* <button onClick={SpeechRecognition.startListening({ language: 'en-US' })}>Start in english</button> */}
+      <button onClick={SpeechRecognition.stopListening} style={{ marginLeft: "8px" }}>Stop</button>
+      <button onClick={resetTranscript} style={{ marginLeft: "8px" }}>Reset</button>
+      <p>{transcript}</p>
+      <button onClick={() => handleQRVlaue({ transcript })}>Generate QR code</button>
+      <div>
+        <br />
+        {isDefinded() ? <div>
+          <h1>QR Code</h1>
+          <p>
+            <QRCode value={"Winner"} />
+          </p>
+        </div> : 'No text found'}
+      </div>
+    </div>
+  );
 }
 
 export default Transcripter;
